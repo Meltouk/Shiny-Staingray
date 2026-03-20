598d39aedd39957faeb75fa604b120831f7a91a1
Meltouk <m3lhdz1426jr@gmail.com>
2026-03-19 22:13:57 -0600

    Update juego.js

--- juego.js
+++ juego.js
@@ -78,7 +78,8 @@ this.bgMusic.play();
     this.physics.add.collider(this.manta,this.pipes,this.gameOver,null,this);
 
     // hitbox
-    
+    this.debugGraphics = this.add.graphics();
+    this.physics.world.createDebugGraphic();
    
 
 }
